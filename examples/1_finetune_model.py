d7f504f3b877c900e44d957db0145996cc741c64
Marek Leibl <marek.leibl@gmail.com>
2024-06-05 13:51:12 +0100

    update

--- examples/1_finetune_model.py
+++ examples/1_finetune_model.py
@@ -101,8 +101,8 @@ def main(cfg: DictConfig):
     sample_classes = []
     epoch_steps, batch_size = None, None # To estimate training data size
 
-    # train_steps = cfg.train_steps
-    train_steps = 10  # For debugging
+    train_steps = cfg.train_steps
+    # train_steps = 10  # For debugging
 
     while grad_steps < train_steps:
         epoch += 1
